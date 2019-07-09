a17f196f20f2e47e49931fea2b30ed133a756fc8
Abhinaw Tiwari <Abhinaw.Tiwari@ril.com>
2019-07-10 00:14:56 +0530

    passing address through command line.

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Kanpur', (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
+const location = process.argv[2];
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (location) {
+    geocode(location, (error, data) => {
         if (error) {
             return console.log(error)
         }
-
-        console.log(data.location)
-        console.log(forecastData)
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    
     })
-
-})
-
+} else {
+    console.log('location not provided')
+}
